e8b6df566715b320d57e6f48f5b74fa15ba7d081
Dario Giovannetti <dev@dariogiovannetti.net>
2019-08-06 19:23:57 +0800

    GeoMapLink

---
+++ src/GeoMapLink.js
@@ -0,0 +1,24 @@
+// This file is part of react-components
+// Copyright (C) 2018-present Dario Giovannetti <dev@dariogiovannetti.net>
+// Licensed under MIT
+// https://github.com/kynikos/lib.js.react-components/blob/master/LICENSE
+
+const {createElement: h, Children} = require('react')
+
+
+module.exports = function GeoMapLink({
+  query,
+  lat = 0,
+  lon = 0,
+  children,
+}) {
+  const coords = [lat, lon].join(',')
+
+  return h('a', {
+    href: window.navigator.platform.match(/iPad|iPhone|iPod/ui) == null
+      ? `geo:${coords}?q=${query || coords}`
+      // TODO: The "geo:" URI scheme doesn't work on Safari yet
+      // TODO: Apparently putting the coordinates in place of 0,0 doesn't work
+      : `maps:0,0?q=${query || coords}`,
+  }, Children.toArray(children))
+}
